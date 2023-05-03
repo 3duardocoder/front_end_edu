051d70864c5067431e6b5bfc9afd2f9663f68db6
3duardocoder <127842074+3duardocoder@users.noreply.github.com>
2023-05-03 10:44:14 -0300

    Update script.js

--- projeto_btn/script.js
+++ projeto_btn/script.js
@@ -1,1 +1,21 @@
 
+var click=0;
+Number(click);
+function clickk(){ 
+redefinir();
+
+
+
+click=click+1;
+console.log(click);
+document.getElementById("cont").textContent=click;
+
+
+
+}
+function redefinir(){
+if (click>=100){
+click=-1;
+console.clear();
+}
+}
